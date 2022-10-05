46e72528875f6019d298ce073954160d94d098bb
demetre1 <demetre.kakhabrishvili.1@btu.edu.ge>
2022-10-05 16:09:15 +0400

    gavaketeb male

---
+++ evenNumbersCounter.kt
@@ -0,0 +1,30 @@
+fun main (){
+
+    println (evenNumberCounter(arrayOf(1,2,3,4,5,6,7)))
+
+}
+
+
+
+
+
+
+
+
+fun evenNumberCounter (x:Array<Int>): Int {
+    var sum = 0;
+    var counter = 0;
+    var sahualo = 0;
+for( num in x){
+    if(num % 2 ==0){
+        sum += num
+        counter ++
+
+    }
+}
+    sahualo = sum / counter
+    return  sahualo;
+
+
+
+}
